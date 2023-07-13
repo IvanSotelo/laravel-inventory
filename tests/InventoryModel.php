561ae56ac13832e28272981a2a0f5d8d700bd8aa
Ivan Sotelo <isotelo@controlla.com.mx>
2023-07-13 16:16:31 -0600

    fix: update package tools

--- tests/InventoryModel.php
+++ tests/InventoryModel.php
@@ -3,7 +3,7 @@
 namespace IvanSotelo\Inventory\Tests;
 
 use Illuminate\Database\Eloquent\Model;
-use IvanSotelo\Inventory\HasInventory;
+use IvanSotelo\Inventory\Traits\HasInventory;
 
 class InventoryModel extends Model
 {
